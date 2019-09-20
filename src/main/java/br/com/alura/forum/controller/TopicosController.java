8e4bbe03f8f6232dc1b200711c62b51b51c0d18a
Kleber Carvalho <klebercarvalho@MacBook-Pro.local>
2019-09-20 11:23:06 -0300

    RestController annotation

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,15 +6,15 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 	
 	@RequestMapping("/topicos")
-	@ResponseBody
 	public List<Topico> lista() {
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
