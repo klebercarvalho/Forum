9fd43cc245215f8d36325ad64ff8afc462a84552
Kleber Carvalho <klebercarvalho@MacBook-Pro.local>
2019-09-23 09:33:27 -0300

    Developing a DTO pattern

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,11 +3,10 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
@@ -15,10 +14,10 @@ import br.com.alura.forum.modelo.Topico;
 public class TopicosController {
 	
 	@RequestMapping("/topicos")
-	public List<Topico> lista() {
+	public List<TopicoDto> lista() {
 		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 		
 	}
 
